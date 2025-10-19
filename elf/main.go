f54db0d827a63e90d8965fb6f4dc39b5ca06b7b8
shaokang.sk <shaokang@wepie.com>
2025-10-19 18:35:00 +0800

    完成指令版本

--- elf/main.go
+++ elf/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"debug/dwarf"
 	"debug/elf"
-	"debug/gosym"
 	"fmt"
 	"os"
 )
+
+// https://www.hitzhangjie.pro/debugger101.io/
 
 func HandleErr(err error) {
 	if err != nil {
@@ -28,16 +30,53 @@ func main() {
 	//	fmt.Println(inst.String())
 	//	offset += inst.Len
 	//}
-	gosymtab, err := file.Section(".gosymtab").Data()
+	//gosymtab, err := file.Section(".gosymtab").Data()
+	//HandleErr(err)
+	//gopclntab, err := file.Section(".gopclntab").Data()
+	//HandleErr(err)
+	//
+	//pclntab := gosym.NewLineTable(gopclntab, file.Section(".text").Addr)
+	//table, err := gosym.NewTable(gosymtab, pclntab)
+	//HandleErr(err)
+	//pc, _, err := table.LineToPC("/root/my_debug/test_main/main.go", 6)
+	//HandleErr(err)
+	//fmt.Println("PC:", pc)
+	//fmt.Println(table.PCToLine(pc))
+	data, err := file.DWARF()
 	HandleErr(err)
-	gopclntab, err := file.Section(".gopclntab").Data()
-	HandleErr(err)
-
-	pclntab := gosym.NewLineTable(gopclntab, file.Section(".text").Addr)
-	table, err := gosym.NewTable(gosymtab, pclntab)
-	HandleErr(err)
-	pc, _, err := table.LineToPC("/root/my_debug/test_main/main.go", 6)
-	HandleErr(err)
-	fmt.Println("PC:", pc)
-	fmt.Println(table.PCToLine(pc))
+	reader := data.Reader()
+	for {
+		entry, err := reader.Next()
+		HandleErr(err)
+		if entry == nil {
+			break
+		}
+		// 编译对象，每个文件都是一个编译对象
+		//if entry.Tag == dwarf.TagCompileUnit {
+		//	temp, err := data.LineReader(entry)
+		//	HandleErr(err)
+		//	for _, item := range temp.Files() {
+		//		if item == nil {
+		//			continue
+		//		}
+		//		fmt.Println("CompileUnit", item.Name)
+		//	}
+		//}
+		//// 编译方法
+		//if entry.Tag == dwarf.TagSubprogram {
+		//	fmt.Println("Subprogram", entry.Val(dwarf.AttrName))
+		//}
+		//// 变量
+		//if entry.Tag == dwarf.TagVariable {
+		//	fmt.Println("Variable", entry.Val(dwarf.AttrName))
+		//}
+		if entry.Tag == dwarf.TagCompileUnit {
+			temp, err := data.LineReader(entry)
+			HandleErr(err)
+			item := dwarf.LineEntry{}
+			err = temp.Next(&item)
+			HandleErr(err)
+			fmt.Println(item) // 获取行号信息
+		}
+	}
 }
--- test_main/main.go
+++ test_main/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type Dog struct {
+	Name string
+}
+
 func main() {
 	fmt.Println("Hello World")
+	d := new(Dog)
+	fmt.Println(d)
 }
